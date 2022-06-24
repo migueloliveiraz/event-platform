872ba937b09442c1005083fe9f9662315f3bb241
migueloliveiraz <migueloliveiradev@gmail.com>
2022-06-24 10:38:48 -0300

    feat: stage 4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-us-west-2.graphcms.com/v2/cl4poj7cp26d601yw5cnc1czj/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
